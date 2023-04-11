7e601cdf446bb4636b457e249012ba219e1f21fd
Ruslan Kuksa <ipz221_kro@student.ztu.edu.ua>
2023-04-12 02:34:59 +0300

    added basic models

---
+++ DurakGame/Models/Deck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurakGame.Models
+{
+    public class Deck
+    {
+        private List<Card> cards;
+        public Deck()
+        {
+            cards = new List<Card>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    cards.Add(new Card { suit, rank });
+                }
+            }
+        }
+    }
+}
