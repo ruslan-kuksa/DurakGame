1ceac182f410163f5e9f5823b76f92db4859f5dc
Ruslan Kuksa <ipz221_kro@student.ztu.edu.ua>
2024-05-24 03:40:55 +0300

    separeted notification

---
+++ DurakGame/Notification/GameNotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurakGame.Messages
+{
+    public static class GameNotification
+    {
+        public const string NoTrumpCardsMessage = "Немає гравців з козирними картами";
+        public const string GameStartedMessage = "Гра розпочалася";
+        public const string BotTurnMessage = "Зараз хід бота, зачекайте своєї черги";
+        public const string PlayerTookCardsMessage = "Гравець взяв карти зі столу";
+        public const string PlayerEndedTurnMessage = "Гравець закінчив свій хід";
+        public const string CardReturnedMessage = "Карта повернута до руки";
+        public const string CannotAddAttackCardMessage = "Цю карту не можливо підкинути";
+        public const string CannotBeatAttackCardMessage = "Ця карта не може побити атакуючу карту";
+    }
+}
